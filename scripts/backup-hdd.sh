492d6182eff10aa41ca3edf22eb90456ac1347f3
AamerSHareef <Aamershareef@gmail.com>
2019-08-15 14:55:44 +0100

    Fix

--- scripts/backup-hdd.sh
+++ scripts/backup-hdd.sh
@@ -5,7 +5,7 @@ function backup_to_hdd(){
 	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."
 	
 	read -p "[!] MIRROR backup operation: This will delete outdated files in the HDD. Ready?"
-	rsync --update -raW --progress --delete ~/* /run/media/zenitsu/Backup_Drive/master
+	rsync --update -raW --progress --delete --exclude='.*' ~/* /run/media/zenitsu/Backup_Drive/master
 	echo "[+] Backup Complete."
 }
 
