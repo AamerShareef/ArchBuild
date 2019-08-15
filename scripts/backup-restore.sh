b641930544725413706ba05845f83e961556f600
AamerSHareef <Aamershareef@gmail.com>
2019-08-15 13:58:55 +0100

    Fix

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -5,6 +5,7 @@ function backup_to_hdd(){
 	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."
 	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/* ~/
 	echo "[+] Backup Complete."
+	fix-perm.sh
 }
 
 backup_to_hdd
