08d0689624711d7ce5c1e736d42cc508a1f1a3d3
AamerShareef <aamershareef@gmail.com>
2019-08-15 10:20:46 +0100

    Added backup restore script

---
+++ scripts/backup-restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function backup_to_hdd(){
+
+	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."
+	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/* ~/
+	echo "[+] Backup Complete."
+}
+
+backup_to_hdd
+
+
