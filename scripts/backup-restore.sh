44ed07f752409144de71b36abcba1e61806b748a
Aamer Shareef <aamershareef@gmail.com>
2019-08-15 16:44:02 +0100

    Update backup-restore.sh

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function restore_from_hdd(){i
+function restore_from_hdd(){
 
 	echo "[-] Restoring backup from HDD"
 	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/* ~/
 	echo "[+] Restore Complete."
-	fix-perm.sh
+	/usr/bin/local/fix-perm.sh
 }
 
 restore_from_hdd
