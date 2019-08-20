d3f46cbea89a88f226c0316adf92cbac54c788c7
AamerShareef <aamershareef@gmail.com>
2019-08-20 20:31:43 +0100

    Update: Folder Changes

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -3,7 +3,7 @@
 function restore_from_hdd(){
 
 	echo "[-] Restoring backup from HDD"
-	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/* ~/
+	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/ ~/
 	echo "[+] Restore Complete."
 	/usr/local/bin/fix-perm.sh
 }
