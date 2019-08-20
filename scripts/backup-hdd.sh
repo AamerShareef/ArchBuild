9a0bf569fea86de66f83eb98f8b6fd091dfe6d25
AamerShareef <aamershareef@gmail.com>
2019-08-20 20:30:28 +0100

    Update: Folder Changes

--- scripts/backup-hdd.sh
+++ scripts/backup-hdd.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 function backup_to_hdd(){
-
-	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."
-	
+	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."	
 	read -p "[!] MIRROR backup operation: This will delete outdated files in the HDD. Ready?"
-	rsync --update -raW --progress --delete --include=".*" --exclude={'.cache','.local'.'.msf4','.mozilla',} ~/* /run/media/zenitsu/Backup_Drive/master
+	rsync --update -raW --progress --delete  --exclude '.bash*' --exclude '.cache' --include '.config/inkdrop'  --exclude '.esd*' --exclude '.local' --exclude '.oh-my-zsh' --exclude '.pki' --exclude '.wget*' --exclude '.zcomp*' ~/ /run/media/zenitsu/Backup_Drive/master
 	echo "[+] Backup Complete."
 }
 
