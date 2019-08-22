c2fbaa95071e614470c0667c9dd8985ebc9debc5
Aamer Shareef <aamershareef@gmail.com>
2019-08-22 21:40:27 +0100

    Added drive

--- scripts/backup-hdd.sh
+++ scripts/backup-hdd.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
+drive=$(lsblk | grep /run | cut -d "/" -f 5)
 function backup_to_hdd(){
 	echo "[-] Starting backup. Backing up all files in home/$USER/* to HDD."	
 	read -p "[!] MIRROR backup operation: This will delete outdated files in the HDD. Ready?"
-	rsync --update -raW --progress --delete  --exclude '.bash*' --exclude '.cache' --include '.config/inkdrop'  --exclude '.esd*' --exclude '.local' --exclude '.oh-my-zsh' --exclude '.pki' --exclude '.wget*' --exclude '.zcomp*' ~/ /run/media/zenitsu/Backup_Drive/master
+	rsync --update -raW --progress --delete  --exclude '.bash*' --exclude '.cache' --include '.config/inkdrop'  --exclude '.esd*' --exclude '.local' --exclude '.oh-my-zsh' --exclude '.pki' --exclude '.wget*' --exclude '.zcomp*' ~/ /run/media/zenitsu/$drive/master
 	echo "[+] Backup Complete."
 }
 
 backup_to_hdd
-
-
