a3bd57fcbba6fe06314dd97cc72937ec4bbb643c
Aamer Shareef <aamershareef@gmail.com>
2019-08-22 21:41:41 +0100

    Added drive

--- scripts/backup-restore.sh
+++ scripts/backup-restore.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+drive=$(lsblk | grep /run | cut -d "/" -f 5)
 function restore_from_hdd(){
 
-	echo "[-] Restoring backup from HDD"
-	rsync --update -raW --progress /run/media/zenitsu/Backup_Drive/master/ ~/
+	read -p "[-] Restoring backup from HDD - $drive - Make sure to connect only the HDD"
+	rsync --update -raW --progress /run/media/zenitsu/$drive/master/ ~/
 	echo "[+] Restore Complete."
 	/usr/local/bin/fix-perm.sh
 }
 
 restore_from_hdd
+
 
 
