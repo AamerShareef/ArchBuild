5ee299c6d03fa79f91d074011f2d2d7a3bdf93cf
aamershareef <aamershareef@gmail.com>
2019-09-01 00:20:25 +0100

    Update

--- installation/arch-workflow.sh
+++ installation/arch-workflow.sh
@@ -65,7 +65,7 @@ function setup_workspace(){
   echo "[-] Creating your workspace $USER!"
   mkdir -p /home/$USER/{void,pwn,space/{notes/{personal,offsec,other},library,projects}}
   mkdir -p /home/$USER/void/{pictures,downloads,videos}
-  mkdir -p /home/$USER/pwn/{binaries/{win,unix},boxes/{oscp,htb},connect,exploits/{win,unix},opt,vm}
+  mkdir -p /home/$USER/pwn/{binaries/{win,unix},boxes/{oscp,htb},labs,exploits/{win,unix},opt,vm}
   rm -rf /home/$USER/D* /home/$USER/Mus* /home/$USER/Pict* /home/$USER/Vid* /home/$USER/Temp* /home/$USER/Publi*
 
   sed -i  's/^\([^#]\)/#\1/g' /home/$USER/.config/user-dirs.dirs
